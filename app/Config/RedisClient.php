f9a2c0a857e8682baa323e0fea47228715aebb89
Jacek Smyda <jacadym@gmail.com>
2025-08-04 21:54:37 +0200

    Add React Redis
    Signed-off-by: Jacek Smyda <jacadym@gmail.com>

---
+++ app/Config/RedisClient.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Config;
+
+use CodeIgniter\Config\BaseConfig;
+
+class RedisClient extends BaseConfig
+{
+    public string $uri = 'redis://localhost:6379';
+
+    public string $prefixKey = '';
+}
--- app/Config/Services.php
+++ app/Config/Services.php
@@ -2,31 +2,23 @@
 
 namespace Config;
 
+use App\Config\RedisClient;
+use Clue\React\Redis\RedisClient as ReactRedisClient;
 use CodeIgniter\Config\BaseService;
 
-/**
- * Services Configuration file.
- *
- * Services are simply other classes/libraries that the system uses
- * to do its job. This is used by CodeIgniter to allow the core of the
- * framework to be swapped out easily without affecting the usage within
- * the rest of your application.
- *
- * This file holds any application-specific services, or service overrides
- * that you might need. An example has been included with the general
- * method format you should use for your service methods. For more examples,
- * see the core Services file at system/Config/Services.php.
- */
 class Services extends BaseService
 {
-    /*
-     * public static function example($getShared = true)
-     * {
-     *     if ($getShared) {
-     *         return static::getSharedInstance('example');
-     *     }
-     *
-     *     return new \CodeIgniter\Example();
-     * }
+    /**
+     * @return ReactRedisClient|object
      */
+    public static function reactredis(bool $getShared = true)
+    {
+        if ($getShared) {
+            return static::getSharedInstance('reactredis');
+        }
+
+        $config = config(RedisClient::class);
+
+        return new ReactRedisClient($config->uri);
+    }
 }
